42fcb7b6ae7199241fb808bcd14ac07ce11efa27
phocom <rtphocom@gmail.com>
2019-12-25 23:25:39 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ function ajax(url, data, success) {
 	let req = new XMLHttpRequest();
 	req.onreadystatechange = function () {
 		if (req.readyState == 4) {
-			console.log(req.responseText);
 			success(JSON.parse(req.responseText));
 		} else {
 
 		}
 	}
-	url += '?';
-	if ('id' in data) {
-		url += `id=${data.id}`;
-	} console.log(url);
-	req.open('GET', url);
-	//req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	req.send();
+	req.open('POST', url);
+	req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+	req.send(EncodeHTMLForm(data));
+}
+
+function EncodeHTMLForm(data) {
+	let params = [];
+	for (let name in data) {
+		let value = data[name];
+		let param = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+		params.push(param);
+	}
+	return params.join('&').replace(/%20/g, '+');
 }
 
 function elm(id) {
